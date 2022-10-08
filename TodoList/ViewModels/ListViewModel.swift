3d773c27071bfc8fa86e835f6101074ad138a33d
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-10-08 21:18:10 +0800

    Added Animations, Transitions

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -11,19 +11,34 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "First title", isCompleted: false),
-            ItemModel(title: "2nd item in the list!", isCompleted: true),
-            ItemModel(title: "3rd item!", isCompleted: false),
-        ]
-        items.append(contentsOf: newItems)
+//        let newItems = [
+//            ItemModel(title: "First title", isCompleted: false),
+//            ItemModel(title: "2nd item in the list!", isCompleted: true),
+//            ItemModel(title: "3rd item!", isCompleted: false),
+//        ]
+//        items.append(contentsOf: newItems)
+        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -51,6 +66,12 @@ class ListViewModel: ObservableObject {
             items[index] = item.updateCompletion()
         }
         
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.setValue(encodedData, forKey: itemsKey)
+        }
     }
     
 }
