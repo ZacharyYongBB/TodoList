59998ffcd712c0c53cd72e806967df3a1cf1b476
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-10-08 09:59:56 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Zachary on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
